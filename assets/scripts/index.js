9954a60d23d1ff08a72c8f1c0fec32697f02508e
jennygodley <jennifergodley@gmail.com>
2019-09-21 19:14:47 -0400

    Update images
    Added additional images and added jQuery script for mouseover.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,12 @@
 $(() => {
-
+  $('.kitty-picture').mouseover(() =>
+    $('.kitty-picture').attr('src', 'assets/images/bad_kitty_word_match3.png').mouseout(() =>
+      $('.kitty-picture').attr('src', 'assets/images/bad_kitty_word_match2.png')))
+  $('.tic-tac-toe-picture').mouseover(() =>
+    $('.tic-tac-toe-picture').attr('src', 'assets/images/tic_tac_toe2.png').mouseout(() =>
+      $('.tic-tac-toe-picture').attr('src', 'assets/images/tic_tac_toe.png')))
 })
+
+// const replaceKitty = function () {
+//   $('.kitty-picture').attr('src', 'assets/images/bad_kitty_word_match3.png').mouseout(() => $('.kitty-picture').attr('src', 'assets/images/bad_kitty_word_match2.png'))
+// }
