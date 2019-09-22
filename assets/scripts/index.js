837538f40c1ee7eb3c217e9b455246e37f7b9803
jennygodley <jennifergodley@gmail.com>
2019-09-22 08:23:33 -0400

    Update navbar
    Added fading in and out of the navbar on scroll, as well as scrollspy.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,11 +2,18 @@ $(() => {
   $('.kitty-picture').mouseover(() =>
     $('.kitty-picture').attr('src', 'assets/images/bad_kitty_word_match3.png').mouseout(() =>
       $('.kitty-picture').attr('src', 'assets/images/bad_kitty_word_match2.png')))
+
   $('.tic-tac-toe-picture').mouseover(() =>
     $('.tic-tac-toe-picture').attr('src', 'assets/images/tic_tac_toe2.png').mouseout(() =>
       $('.tic-tac-toe-picture').attr('src', 'assets/images/tic_tac_toe.png')))
+
   $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip()
+  })
+
+  $(window).scroll(function () {
+    const offset = $(window).scrollTop()
+    $('.navbar').toggleClass('nottrans', offset > 300)
   })
 })
 
